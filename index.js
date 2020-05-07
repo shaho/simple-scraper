c821a8b12e738ed87e842259f27eb934711d53db
Shaho Toofani <toofani@gmail.com>
2020-05-07 10:41:01 +0430

    add cheerio

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ async function main() {
   const html = await request.get(
     "https://reactnativetutorial.net/css-selectors/",
   );
-
   fs.writeFileSync("./test.html", html);
+
+  const $ = await cheerio.load(html);
+  const theText = $("h1").text();
+  console.log(theText);
 }
 
 main();
